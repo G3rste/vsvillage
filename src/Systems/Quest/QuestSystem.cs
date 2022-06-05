d75681735c6d0a94e39572ae3b6f654469ad4422
G3rste <philippgerstenberg@web.de>
2022-06-06 00:28:52 +0200

    more quests

--- src/Systems/Quest/QuestSystem.cs
+++ src/Systems/Quest/QuestSystem.cs
@@ -40,60 +40,86 @@ namespace VsVillage
 
         }
 
+        private List<Objective> createSimpleObjective(string[] objective, int demand)
+        {
+            return new List<Objective>(new Objective[] { new Objective() { validCodes = new List<string>(objective), demand = demand } });
+        }
+
+        private List<ItemReward> createSimpleReward(string itemCode, int amount)
+        {
+            return new List<ItemReward>(new ItemReward[] { new ItemReward() { itemCode = itemCode, amount = amount } });
+        }
+
         private List<Quest> getDefaultQuests()
         {
-            var kill15drifters = new Quest()
+            var kill4Wolves = createSimpleObjective(new string[] { "wolf-male", "wolf-female", "wolf-pup" }, 4);
+            var kill15Drifters = createSimpleObjective(new string[] { "drifter-normal", "drifter-deep", "drifter-tainted", "drifter-corrupt", "drifter-nightmare", "drifter-double-headed" }, 15);
+            var kill3NightmareDrifters = createSimpleObjective(new string[] { "drifter-nightmare", "drifter-double-headed" }, 3);
+            var gather20Meat = createSimpleObjective(new string[] { "bushmeat-raw", "bushmeat-cooked", "bushmeat-cured", "redmeat-raw", "redmeat-cooked", "redmeat-cured", "poultry-raw", "poultry-cooked", "poultry-cured" }, 20);
+            var gather20CharCoal = createSimpleObjective(new string[] { "charcoal" }, 20);
+
+            var for3Gears = createSimpleReward("game:gear-rusty", 3);
+            var for5Gears = createSimpleReward("game:gear-rusty", 5);
+
+            var for8FlaxSeeds = createSimpleReward("game:seeds-flax", 8);
+            var for8ParsnipSeeds = createSimpleReward("game:seeds-parsnip", 8);
+            var for8TurnipSeeds = createSimpleReward("game:seeds-turnip", 8);
+
+            var kill15DriftersFor3Gears = new Quest()
             {
-                id = "vsvillage:quest-kill15drifters",
+                id = "vsvillage:quest-kill15DriftersFor3Gears",
                 gatherObjectives = new List<Objective>(),
-                killObjectives = new List<Objective>(
-                    new Objective[]
-                    {
-                        new Objective()
-                        {
-                            validCodes = new List<string>(new string[] { "drifter-normal", "drifter-deep", "drifter-tainted", "drifter-corrupt", "drifter-nightmare", "drifter-double-headed" }),
-                            demand = 15
-                        }
-                    }),
+                killObjectives = kill15Drifters,
                 actionRewardIds = new List<string>(),
-                itemRewards = new List<ItemReward>(new ItemReward[] { new ItemReward() { itemCode = "game:gear-rusty", amount = 3 } })
+                itemRewards = for3Gears
             };
 
-            var kill4wolves = new Quest()
+            var kill3NightmareDriftersFor5Gears = new Quest()
             {
-                id = "vsvillage:quest-kill4wolves",
+                id = "vsvillage:quest-kill3NightmareDriftersFor5Gears",
                 gatherObjectives = new List<Objective>(),
-                killObjectives = new List<Objective>(
-                    new Objective[]
-                    {
-                        new Objective()
-                        {
-                            validCodes = new List<string>(new string[] { "wolf-male", "wolf-female", "wolf-pup" }),
-                            demand = 4
-                        }
-                    }),
+                killObjectives = kill3NightmareDrifters,
                 actionRewardIds = new List<string>(),
-                itemRewards = new List<ItemReward>(new ItemReward[] { new ItemReward() { itemCode = "game:gear-rusty", amount = 3 } })
+                itemRewards = for5Gears
             };
 
-            var gather20meat = new Quest()
+            var kill4WolvesFor3Gears = new Quest()
             {
-                id = "vsvillage:quest-gather20meat",
-                gatherObjectives = new List<Objective>(
-                    new Objective[]
-                    {
-                        new Objective()
-                        {
-                            validCodes = new List<string>(new string[] { "bushmeat-raw", "bushmeat-cooked", "bushmeat-cured", "redmeat-raw", "redmeat-cooked", "redmeat-cured", "poultry-raw", "poultry-cooked", "poultry-cured" }),
-                            demand = 20
-                        }
-                    }),
+                id = "vsvillage:quest-kill4WolvesFor3Gears",
+                gatherObjectives = new List<Objective>(),
+                killObjectives = kill4Wolves,
+                actionRewardIds = new List<string>(),
+                itemRewards = for3Gears
+            };
+
+            var gather20MeatFor8FlaxSeeds = new Quest()
+            {
+                id = "vsvillage:quest-gather20MeatFor8FlaxSeeds",
+                gatherObjectives = gather20Meat,
                 killObjectives = new List<Objective>(),
                 actionRewardIds = new List<string>(),
-                itemRewards = new List<ItemReward>(new ItemReward[] { new ItemReward() { itemCode = "game:seeds-flax", amount = 8 } })
+                itemRewards = for8FlaxSeeds
             };
 
-            return new List<Quest>(new Quest[] { kill15drifters, kill4wolves, gather20meat });
+            var gather20MeatFor8ParsnipSeeds = new Quest()
+            {
+                id = "vsvillage:quest-gather20MeatFor8ParsnipSeeds",
+                gatherObjectives = gather20Meat,
+                killObjectives = new List<Objective>(),
+                actionRewardIds = new List<string>(),
+                itemRewards = for8ParsnipSeeds
+            };
+
+            var gather20MeatFor8TurnipSeeds = new Quest()
+            {
+                id = "vsvillage:quest-gather20MeatFor8TurnipSeeds",
+                gatherObjectives = gather20Meat,
+                killObjectives = new List<Objective>(),
+                actionRewardIds = new List<string>(),
+                itemRewards = for8TurnipSeeds
+            };
+
+            return new List<Quest>(new Quest[] { kill15DriftersFor3Gears, kill3NightmareDriftersFor5Gears, kill4WolvesFor3Gears, gather20MeatFor8FlaxSeeds, gather20MeatFor8ParsnipSeeds, gather20MeatFor8TurnipSeeds });
         }
     }
 }
