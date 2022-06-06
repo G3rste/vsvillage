4f352bd7434e71001314539bf1eb498c5e2e98be
G3rste <philippgerstenberg@web.de>
2022-06-06 20:52:24 +0200

    add more quest attributes

--- src/Systems/Quest/Quest.cs
+++ src/Systems/Quest/Quest.cs
@@ -5,6 +5,9 @@ namespace VsVillage
     public class Quest
     {
         public string id { get; set; }
+        public int cooldown { get; set; }
+        public bool perPlayer { get; set; }
+        public List<string> prerequesits { get; set; }
         public List<Objective> gatherObjectives { get; set; }
         public List<Objective> killObjectives { get; set; }
         public List<ItemReward> itemRewards { get; set; }
