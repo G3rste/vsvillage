85641f2ac87ae84e9520671e4507f886a2be38ef
G3rste <philippgerstenberg@web.de>
2022-07-03 16:02:45 +0200

    reference vsquest in release file name

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 version=$(grep -o '"version":\s*"\([0-9]\.*\)*"' resources/modinfo.json | grep -o '\([0-9]\.*\)*')
-releasefile='bin/vsvillage_v'$version'.zip'
+vsquest=$(grep -o '"vsquest":\s*"\([0-9]\.*\)*"' resources/modinfo.json | grep -o '\([0-9]\.*\)*' | sed -r 's/[0-9]*$/+/')
+releasefile='bin/vsvillage_v'$version'_vsquest_v'$vsquest'.zip'
 dotnet build -c release
 mv bin/vsvillage.zip $releasefile
 gh release create --generate-notes 'v'$version $releasefile 
