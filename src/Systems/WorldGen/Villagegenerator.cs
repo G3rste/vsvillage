65ada0718ee72a36a3dd1a9e45f7e848b076bb3f
G3rste <philippgerstenberg@web.de>
2022-07-17 14:01:51 +0200

    add village generator stub

---
+++ src/Systems/WorldGen/Villagegenerator.cs
@@ -0,0 +1,26 @@
+using System;
+using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.Server;
+
+namespace VsVillage{
+    public class VillageGenerator : ModSystem {
+
+        public override double ExecuteOrder() => 0.6;
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.Event.ChunkColumnGeneration(handler, EnumWorldGenPass.TerrainFeatures, "standard");
+            api.Event.MapRegionGeneration(mapHandler, "standard");
+        }
+
+        private void mapHandler(IMapRegion mapRegion, int regionX, int regionZ)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void handler(IServerChunk[] chunks, int chunkX, int chunkZ, ITreeAttribute chunkGenParams)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
