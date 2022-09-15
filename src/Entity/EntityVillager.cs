c46fbdf2d1917962f361af7799ffb9b2069f15d1
G3rste <philippgerstenberg@web.de>
2022-09-15 19:44:15 +0200

    Only interact with player if alive

--- src/Entity/EntityVillager.cs
+++ src/Entity/EntityVillager.cs
@@ -69,7 +69,7 @@ namespace VsVillage
         public override void OnInteract(EntityAgent byEntity, ItemSlot slot, Vec3d hitPosition, EnumInteractMode mode)
         {
             base.OnInteract(byEntity, slot, hitPosition, mode);
-            if (byEntity is EntityPlayer player && WatchedAttributes.GetString("guardedPlayerUid") == player.PlayerUID && mode == EnumInteractMode.Interact && !player.Controls.Sneak)
+            if (Alive && byEntity is EntityPlayer player && WatchedAttributes.GetString("guardedPlayerUid") == player.PlayerUID && mode == EnumInteractMode.Interact && !player.Controls.Sneak)
             {
                 bool commandSit = WatchedAttributes.GetBool("commandSit", false);
                 WatchedAttributes.SetBool("commandSit", !commandSit);
@@ -79,7 +79,7 @@ namespace VsVillage
 
         public override WorldInteraction[] GetInteractionHelp(IClientWorldAccessor world, EntitySelection es, IClientPlayer player)
         {
-            if (WatchedAttributes.GetString("guardedPlayerUid") == player.PlayerUID)
+            if (WatchedAttributes.GetString("guardedPlayerUid") == player.PlayerUID && Alive)
             {
                 string actionLangCode = WatchedAttributes.GetBool("commandSit", false) ? "vsvillage:command-follow" : "vsvillage:command-stay";
                 return new WorldInteraction[]{
