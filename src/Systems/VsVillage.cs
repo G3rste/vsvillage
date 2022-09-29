8f3f8fe661f2aa456dff0b27e7cfdb5ae76bd6ac
G3rste <philippgerstenberg@web.de>
2022-09-29 22:11:57 +0200

    herbalists heal wounded villagers

--- src/Systems/VsVillage.cs
+++ src/Systems/VsVillage.cs
@@ -44,6 +44,7 @@ namespace VsVillage
             AiTaskRegistry.Register<AiTraskVillagerCultivateCrops>("villagercultivatecrops");
             AiTaskRegistry.Register<AiTraskVillagerFlipWeapon>("villagerflipweapon");
             AiTaskRegistry.Register<AiTaskStayCloseToEmployer>("villagerstayclose");
+            AiTaskRegistry.Register<AiTraskHealWounded>("villagerhealwounded");
         }
 
         public override void StartClientSide(ICoreClientAPI api)
