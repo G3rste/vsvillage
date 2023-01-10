30eab04b487060e83cc391b9142f43978f866b40
G3rste <philippgerstenberg@web.de>
2023-01-10 21:56:48 +0100

    more weapons for soldiers

--- src/Item/ItemVillagerGear.cs
+++ src/Item/ItemVillagerGear.cs
@@ -25,6 +25,6 @@ namespace VsVillage
     }
     public enum VillagerGearType
     {
-        HEAD, BACK, CHEST, SHOULDERS, BELT, BELTSLOT, ARMS, THIGH, FEET
+        HEAD, BACK, CHEST, SHOULDERS, BELT, BELTSLOT, ARMS, THIGH, FEET, WEAPON
     }
 }
