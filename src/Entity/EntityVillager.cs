fc6c421620d6ca4302644059c2278358202f1635
G3rste <philippgerstenberg@web.de>
2024-05-25 20:55:14 +0200

    soldiers can only be balanced or rowdy, other animations look stupid

--- src/Entity/EntityVillager.cs
+++ src/Entity/EntityVillager.cs
@@ -57,7 +57,14 @@ namespace VsVillage
             }
             if (!WatchedAttributes.HasAttribute("personality"))
             {
-                Personality = EntityTrader.Personalities.GetKeyAtIndex(World.Rand.Next(EntityTrader.Personalities.Count));
+                if (Enum.Parse<EnumVillagerProfession>(properties.Attributes["profession"].AsString()) == EnumVillagerProfession.soldier)
+                {
+                    Personality = World.Rand.Next(2) == 0 ? "balanced" : "rowdy";
+                }
+                else
+                {
+                    Personality = Personalities.GetKeyAtIndex(World.Rand.Next(EntityTrader.Personalities.Count));
+                }
             }
             (AnimManager as TraderAnimationManager).Personality = Personality;
             if (api is ICoreClientAPI capi) { talkUtil = new EntityTalkUtil(capi, this); }
